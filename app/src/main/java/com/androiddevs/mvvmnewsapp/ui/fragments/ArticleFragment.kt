c6b0029202264b980d9d43e47e56fdd27eb04bfc
Ilya <felagunt@live.ru>
2022-10-09 21:40:07 +0300

    navigation

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -3,6 +3,6 @@ package com.androiddevs.mvvmnewsapp.ui.fragments
 import androidx.fragment.app.Fragment
 import com.androiddevs.mvvmnewsapp.R
 
-class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
-
+class ArticleFragment: Fragment(R.layout.fragment_article) {
+    
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
@@ -3,6 +3,6 @@ package com.androiddevs.mvvmnewsapp.ui.fragments
 import androidx.fragment.app.Fragment
 import com.androiddevs.mvvmnewsapp.R
 
-class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
-
+class SearchNewsFragment: Fragment(R.layout.fragment_search_news) {
+    
 }
