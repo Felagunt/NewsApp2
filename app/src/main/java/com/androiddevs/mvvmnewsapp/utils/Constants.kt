4d264b0137f8d8860d67b12f34b9b871f568b642
Ilya <felagunt@live.ru>
2022-10-09 22:55:53 +0300

    setup retrofit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object {
+        const val API_KEY = "91353fad465b4e38ada534c970935b18"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
